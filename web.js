1b7849a375a18805ea3d983acc40f178dcae29f0
Matt Witherow <hi@mattwitherow.com>
2014-04-24 16:32:09 +1000

    removed ntwitter and got basic output working

--- web.js
+++ web.js
@@ -1,15 +1,42 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
-var app = express();
+var twitter = require("mtwitter");
+//var twittup	= require("./twittup");
 
+var app = express();
 app.use(logfmt.requestLogger());
 
+mTwit = new twitter({
+	consumer_key: 'RG7hca16EGx7OtAQ8JXRfJ5I8',
+	consumer_secret: 'f8xIE16p4ol2AZhxGwITV3rdtQ1l2JifGOV1IhDs6zwRJJlV19',
+	access_token_key: '635348000-17rt0Ycpij1s20Zf9QWN9ASBgf5YK4O6zl9fl4I8',
+	access_token_secret: 'tmfQCg6NxC7JnnpLdh6H1vqb63dUos6xk00jS5zjY6sRX'
+});
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+
+mTwit.get('search/tweets',
+	{	
+		q: "i feel", 
+		geocode: "-37.777,144.971,50km",
+		count: "5",
+		result_type:"recent"
+
+	},
+	function(err, item) {
+		var map = item.statuses.map(function(status){
+			var id = status["id_str"];
+			var text = status["text"];
+			return  { id : id, text: text } ;
+		});
+		console.log(map);
+		res.send(map);
+	});
+
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
